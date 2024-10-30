6eed46ee1cb0da8a8e2e258650d9e0865142dff8
olg2olg <olga17573@gmail.com>
2024-10-30 16:07:29 +0300

    Module4.Part1:task1-4

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,11 +1,19 @@
 #from django.contrib import admin
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home),
-    path('about', views.about),
-    path("item/<int:item_id>", views.get_item),
-    path('items', views.items_list),
-]
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path("item/<int:item_id>", views.get_item, name='item-detail'),
+    path('items', views.items_list, name='items-list'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 #path('admin/', admin.site.urls),
+
+# path('', views.home),
+# path('about', views.about),
+# path("item/<int:item_id>", views.get_item),
+# path('items', views.items_list),
